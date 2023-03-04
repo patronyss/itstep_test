1529a37a407e0512ef194b832f462060b3ff5127
patronyss <buchka-2000>
2023-03-04 13:51:42 +0200

    classwork

---
+++ lesson2.py
@@ -0,0 +1,29 @@
+#ООП. ВСТУП
+class Student:
+    count_of_students = 0
+
+    def say_hi(self):
+        print(f'Hi! I`m {self.name}')
+
+    def __init__(self, name, height):
+        self.name = name
+        self.height = height
+        Student.count_of_students += 1
+
+    def __del__(self):
+        print('Bye!')
+
+    def __str__(self):
+        return f'I`m student {self.name}'
+
+
+sergiy = Student('Sergiy', 180)
+anton = Student('Anton', 160)
+
+sergiy.say_hi()
+anton.say_hi()
+
+print(sergiy.height)
+print(anton.height)
+print(Student.count_of_students)
+print(anton)
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-for i in range(10):
-    print('Hello world')
+
 
 
